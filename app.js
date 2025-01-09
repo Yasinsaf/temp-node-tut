d86cdfca5c8c4b939c79e07e78117b32efd4c7a8
Yasinsaf <yasinsfsf@gmail.com>
2025-01-10 00:51:24 +0300

    my first commite

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("welcome to our home page");
-    res.end();
-  }
-  if (req.url === "/about") {
-    res.write("here is our short history");
-    res.end();
-  } else {
-    res.end(`
-      <h1>Oops!</h1>
-      <p>we cannot seem to find the page you are looking for</p>
-      <a href="/">back home</a>
-    `);
-  }
+  console.log("request event");
+  res.end("hello world");
 });
 
-server.listen(3000);
+server.listen(3001, () => {
+  console.log("server listening on port 3001...");
+});
